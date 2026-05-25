d69febb8e74de8d9d413310fab3c0ea27fdcde02
Cheryl Salsabilla Hendarto <cherylsalsabillahh@gmail.com>
2026-05-25 20:47:39 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiDBController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,7 +18,7 @@ Route::get('blog', function () {
 
 //Tugas
 Route::get('pert-1', function () {
-    return view('pertemuan5');
+    return view('pertemuan1');
 });
 Route::get('pert-2', function () {
     return view('pertemuan5');
@@ -33,5 +34,17 @@ Route::get('pert-5', function () {
 });
 
 
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
+
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+//crud tabel pegawai
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawaitambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawaistore', [PegawaiDBController::class, 'store']);
+Route::get('/pegawaiedit/{id}', [PegawaiDBController::class, 'edit']);
+Route::post('/pegawaiupdate', [PegawaiDBController::class, 'update']);
+Route::get('/pegawaihapus/{id}', [PegawaiDBController::class, 'hapus']);
+Route::get('/pegawaicari', [PegawaiDBController::class, 'cari']);
